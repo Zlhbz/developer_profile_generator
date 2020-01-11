a1a276ed7dfbb5d4903c3e2de698cca8c0ea611b
Zeliha Gencel <zelihagencel@gmail.com>
2020-01-10 21:26:28 -0600

    homework initialized

---
+++ index.js
@@ -0,0 +1,19 @@
+const api = require("./api");
+const html = require("./generateHTML");
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+
+
+
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+
+}
+
+function init() {
+
+    init();
